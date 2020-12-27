5c0456b4d0b541d66137d75e418f4ad1e15a2b7a
Kamalesh31-Kss <76644337+Kamalesh31-Kss@users.noreply.github.com>
2020-12-27 14:31:49 +0530

    AccessData

---
+++ AccessData.py
@@ -0,0 +1,18 @@
+# Demonstrate how to access and perform operations on a main file
+#run the MODULE of MAIN FILE and import mainfile as a library 
+
+import code as x 
+x.create("kamalesh",25)
+x.create("src",70,3600) 
+x.read("kamalesh")
+x.read("src")
+x.create("kamalesh",50)
+x.modify("kamalesh",55)
+x.delete("kamalesh")
+t1=Thread(target=(create or read or delete),args=(key_name,value,timeout))
+t1.start()
+t1.sleep()
+t2=Thread(target=(create or read or delete),args=(key_name,value,timeout))
+t2.start()
+t2.sleep()
+#and so on upto tn
