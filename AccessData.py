558a3957afcfaaee2f6f30d3684c6f6d39c1ba97
Kamalesh31-Kss <76644337+Kamalesh31-Kss@users.noreply.github.com>
2020-12-27 14:33:04 +0530

    Update AccessData.py

--- AccessData.py
+++ AccessData.py
@@ -1,7 +1,7 @@
 # Demonstrate how to access and perform operations on a main file
 #run the MODULE of MAIN FILE and import mainfile as a library 
 
-import code as x 
+import mainprog as x 
 x.create("kamalesh",25)
 x.create("src",70,3600) 
 x.read("kamalesh")
